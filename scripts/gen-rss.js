c03d8db215d10c7f6c1e4e239bb2950ef02b8bc3
Carlos Michael <carlosmichaeldev@gmail.com>
2024-05-15 01:21:06 -0400

    Update RSS feed information with correct site URL and title

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require("gray-matter");
 
 async function generate() {
   const feed = new RSS({
-    title: "Your Name",
-    site_url: "https://yoursite.com",
-    feed_url: "https://yoursite.com/feed.xml",
+    title: "carlos-michael",
+    site_url: "https://carlos-michael.com",
+    feed_url: "https://carlos-michael.com/feed.xml",
   });
 
   const posts = await fs.readdir(path.join(__dirname, "..", "pages", "posts"));
